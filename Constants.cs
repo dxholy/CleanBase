75e97fda13c67bd3bbfc8c3d263b36c9768ead5c
dxholy <dax_thomas@yahoo.com>
2016-01-26 12:02:36 +0530

    Create Constants.cs

---
+++ Constants.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+
+public class Constants : MonoBehaviour {
+	
+	
+	
+	
+	
+	public static Vector4 red= new Vector4(1,0,0,1);
+	public static Vector4 green= new Vector4(0,1,0,1);
+	public static Vector4 blue= new Vector4(0,0,1,1);
+	public static Vector4 purple= new Vector4(1,0,1,1);
+
+	
+	public static Vector4 white= new Vector4(1,1,1,1);
+	public static Vector4 cyan= new Vector4(0,1,1,1);
+	public static Vector4 gray= new Vector4(0.1f,0.1f,0.2f,1f);
+	public static Vector4 black= new Vector4(0.0f,0.0f,0.0f,1f);
+
+
+	public static Vector4 softblue= new Vector4(0.3f,0.3f,0.7f,1f);
+	public static Vector4 pink= new Vector4(0.7f,0,0.7f,1);
+	
+	
+	
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+		
+		//GameObject.Find("Dock").GetComponent<Renderer>().material.color =  blue;
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
